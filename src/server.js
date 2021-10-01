2a2180835547bf10b8e28babe5b746e3037ff171
Bhawani Shankar <bhaweshlohar2@gmail.com>
2021-10-01 17:28:50 +0530

    Axios basic  test & working fine

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const connect = require('./configs/db')
 const app = new express();
-// const bodyParser = require("body-parser");
-// const session = require('express-session');
-// const flash = require('connect-flash');
+// // const bodyParser = require("body-parser");
+// // const session = require('express-session');
 
 app.use(express.json());
+
+const testRoute = require('./routes/test.route')
+app.use('/user', testRoute);
+
 
 app.listen(5000, async () => {
     await connect();
